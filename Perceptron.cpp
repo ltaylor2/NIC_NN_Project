a3c947aeac5743ed0cb4b2240205e4500e6f8433
Liam <ltaylor.ulysses@gmail.com>
2015-04-14 17:34:32 -0400

    It makes no sense! what you're doing is you have a hyperplane and and and and

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -6,65 +6,77 @@
 #include <cmath>
 
 Perceptron::Perceptron(int epochs, double learningRate, const std::vector<std::pair<std::vector<double>, std::vector<double>>>& labeled)
-    : inputLayer(labeled[0].first.size(), Node()),
-      outputLayer(labeled[0].second.size(), Node())
+    : outputLayer(labeled[0].second.size(), Node())
 {
     srand(time(NULL));
+    
+    int inputNum = labeled[0].first.size();
 
-    weights = new double*[inputLayer.size()];
-    for (unsigned int i = 0; i < inputLayer.size(); i++)
+    weights = new double*[inputNum];
+    for (int i = 0; i < inputNum; i++) {
         weights[i] = new double[outputLayer.size()];
+    }
 
-    for (unsigned int i = 0; i < inputLayer.size(); i++) {
+    std::cout << "inputNum: " << inputNum << std::endl;
+
+    for (int i = 0; i < inputNum; i++) {
         for (unsigned int j = 0; j < outputLayer.size(); j++) {
             weights[i][j] = 2*(static_cast<double>(rand()) / RAND_MAX) - 1;
+            std::cout << weights[i][j] << std::endl;
         }
     }
 
     train(epochs, learningRate, labeled);
+
 }
 
 // NOTE assumes output is correct size
 void Perceptron::evaluate(const std::vector<double>& input, std::vector<double>& output)
 {
-    for (unsigned int i = 0; i < inputLayer.size(); i++)
-        inputLayer[i].evaluateNode(input[i]);
-
     for (unsigned int i = 0; i < outputLayer.size(); i++) {
-        std::vector<double> inputs(inputLayer.size(), 0);
+        std::vector<double> weightedInputs(input.size(), 0);
 
-        for (unsigned int j = 0; j < inputLayer.size(); j++) {
-            inputs[j] = weights[j][i]*inputLayer[j].getOutput();
+        for (unsigned int j = 0; j < input.size(); j++) {
+            weightedInputs[j] = weights[j][i]*input[j];
         }
 
-        output[i] = 10*outputLayer[i].evaluateNode(inputs);
+        //output[i] = outputLayer[i].evaluateNode(weightedInputs);
+        output[i] = floor(10 * outputLayer[i].evaluateNode(weightedInputs));
     }
 }
 
 void Perceptron::train(int epochs, double learningRate, const std::vector<std::pair<std::vector<double>, std::vector<double>>>& labeled)
 {
+        std::cout << learningRate << std::endl;
+
     for (int i = 0; i < epochs; i++) {
         std::cout << "epoch, " << i << std::endl;
+        double totalError = 0;
         for (unsigned int j = 0; j < labeled.size(); j++) {
             std::vector<double> output(labeled[j].second.size(), 0);
             evaluate(labeled[j].first, output);
 
             double error = 0;
             for (unsigned int k = 0; k < output.size(); k++) {
-                error += fabs(10*output[k] - labeled[j].second[k]);
+                error += labeled[j].second[k] - output[k];
             }
+            totalError += error;
 
             if (error != 0) {
-                for (unsigned int m = 0; m < inputLayer.size(); m++) {
+                for (unsigned int m = 0; m < labeled[j].first.size(); m++) {
                     for (unsigned int n = 0; n < outputLayer.size(); n++) {
-                        // TODO check weight update
-                        // 
                         weights[m][n] += learningRate * error * sigmoidPrime(labeled[j].first[m]);
                     }
                 }
             }
         }
+        std::cout << totalError << std::endl;
     }
+    // for (unsigned int m = 0; m < labeled[0].first.size(); m++) {
+    //     for (unsigned int n = 0; n < outputLayer.size(); n++) {
+    //         std::cout << weights[m][n] << std::endl;
+    //     }
+    // }
 }
 
 double Perceptron::sigmoidPrime(double x) {
